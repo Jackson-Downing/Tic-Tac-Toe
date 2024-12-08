5e30799edbe193c3fd1ce197c9307ae957be94b8
Jackson-Downing <jacksondowning2@outlook.com>
2024-12-08 18:55:59 +0930

    Added btns
    Bare bones game buttons + used loop to create grid

--- main.py
+++ main.py
@@ -5,9 +5,18 @@ root.title("Tic Tac Toe - Jackson Downing")
 root.geometry("500x600")
 
 gameGrid = tk.Frame(root)
-gameGrid.grid_columnconfigure(0, weight=1)
-gameGrid.grid_columnconfigure(1, weight=1)
-gameGrid.grid_columnconfigure(2, weight=1)
-gameGrid.pack(padx=10, pady=10)
+for i in range(3):
+    gameGrid.grid_columnconfigure(i, weight=1, uniform="equal")
+    gameGrid.grid_rowconfigure(i, weight=1, uniform="equal")
+gameGrid.pack(padx=20, pady=20, expand=True, fill="both")
+
+gameBtns = []
+for r in range(3):
+    for c in range(3):
+        btn = tk.Button(gameGrid, text="Wow")
+        btn.grid(row=r, column=c, sticky="news")
+        gameBtns.append(btn)
+
+
 
 root.mainloop()
